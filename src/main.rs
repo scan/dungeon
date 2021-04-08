3714b629b8da4abd0fce9962de0f66c9c97bc399
Tom Streller <tom@streller.me>
2021-04-08 09:21:50 +0200

    Fully migrated

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn animate_sprite_system(
   )>,
 ) {
   for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
-    timer.tick(time.delta_seconds());
+    timer.tick(time.delta());
     if timer.finished() {
       let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
       sprite.index =
@@ -33,7 +33,7 @@ fn animate_sprite_system(
 }
 
 fn setup(
-  commands: &mut Commands,
+  mut commands: Commands,
   asset_server: Res<AssetServer>,
   mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
@@ -42,21 +42,21 @@ fn setup(
     TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 32.0), 4, 4);
   let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
+  commands.spawn_bundle(OrthographicCameraBundle::new_2d());
   commands
-    .spawn(Camera2dBundle::default())
-    .spawn(SpriteSheetBundle {
+    .spawn_bundle(SpriteSheetBundle {
       texture_atlas: texture_atlas_handle,
       transform: Transform::from_scale(Vec3::splat(4.0)),
       ..Default::default()
     })
-    .with(Timer::from_seconds(0.1, true));
+    .insert(Timer::from_seconds(0.1, true));
 }
 
 fn setup_tilemap(asset_server: Res<AssetServer>) {
   let texture_handle = asset_server.load("tiles/tile.png");
   if let Ok(_tilemap) = Tilemap::builder()
     .texture_atlas(texture_handle)
-    .tile_dimensions(32, 32)
+    .texture_dimensions(32, 32)
     .dimensions(40, 30)
     .finish()
   {}
