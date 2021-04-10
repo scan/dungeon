2006442ddbe481907d0410d7e970a90abfb52705
Tom Streller <tom@streller.me>
2021-04-10 13:20:54 +0200

    Split image for loading

--- src/loading.rs
+++ src/loading.rs
@@ -1,30 +1,41 @@
-use anyhow::{Error, Result};
 use bevy::{asset::LoadState, prelude::*, sprite::TextureAtlasBuilder};
+use bevy_tilemap::{
+  prelude::{LayerKind, TilemapBundle},
+  Tilemap, TilemapLayer,
+};
+
+pub struct LoadingScreenPlugin;
+impl Plugin for LoadingScreenPlugin {
+  fn build(&self, app: &mut AppBuilder) {
+    app
+      .init_resource::<TileSpriteHandles>()
+      .add_startup_system(setup.system())
+      .add_system(load.system());
+  }
+}
 
 #[derive(Clone, Default, Debug)]
-pub struct TileSpriteHandles {
+struct TileSpriteHandles {
   handles: Vec<HandleUntyped>,
   loaded: bool,
 }
 
-pub fn setup(
+fn setup(
   mut tile_sprite_handles: ResMut<TileSpriteHandles>,
   asset_server: Res<AssetServer>,
-) -> Result<()> {
-  tile_sprite_handles.handles = asset_server.load_folder("tiles")?;
-
-  Ok(())
+) {
+  tile_sprite_handles.handles = asset_server.load_folder("tiles").unwrap();
 }
 
-pub fn load(
+fn load(
   mut commands: Commands,
   mut sprite_handles: ResMut<TileSpriteHandles>,
   mut texture_atlases: ResMut<Assets<TextureAtlas>>,
   mut textures: ResMut<Assets<Texture>>,
   asset_server: Res<AssetServer>,
-) -> Result<()> {
+) {
   if sprite_handles.loaded {
-    return Ok(());
+    return;
   }
 
   let mut texture_atlas_builder = TextureAtlasBuilder::default();
@@ -32,15 +43,48 @@ pub fn load(
     .get_group_load_state(sprite_handles.handles.iter().map(|h| h.id))
   {
     for handle in sprite_handles.handles.iter() {
-      let texture = textures
-        .get(handle)
-        .ok_or(Error::msg("texture not found"))?;
+      let texture = textures.get(handle).unwrap();
+      debug!("Loaded texture: {:?}", handle);
       texture_atlas_builder.add_texture(handle.clone_weak().typed(), texture);
     }
 
-    let texture_atlas = texture_atlas_builder.finish(&mut textures)?;
+    let texture_atlas = texture_atlas_builder.finish(&mut textures).unwrap();
     let atlas_handle = texture_atlases.add(texture_atlas);
-  }
 
-  Ok(())
+    let tilemap = Tilemap::builder()
+      .dimensions(16 * 50, 16 * 50)
+      .chunk_dimensions(16, 16, 1)
+      .texture_dimensions(32, 32)
+      .auto_chunk()
+      .auto_spawn(2, 2)
+      .add_layer(
+        TilemapLayer {
+          kind: LayerKind::Dense,
+          ..Default::default()
+        },
+        0,
+      )
+      .texture_atlas(atlas_handle)
+      .finish()
+      .unwrap();
+
+    let tilemap_components = TilemapBundle {
+      tilemap,
+      visible: Visible {
+        is_visible: true,
+        is_transparent: true,
+      },
+      transform: Default::default(),
+      global_transform: Default::default(),
+    };
+    commands
+      .spawn()
+      .insert_bundle(OrthographicCameraBundle::new_2d());
+    commands
+      .spawn()
+      .insert_bundle(tilemap_components)
+      .insert(Timer::from_seconds(0.075, true));
+
+    sprite_handles.loaded = true;
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,57 +5,19 @@ use bevy_tilemap::prelude::*;
 
 fn main() {
   App::build()
-    .add_plugins(DefaultPlugins)
-    .add_startup_system(loading::setup.system())
-    .add_system(loading::load.system())
-    .run();
-}
-
-fn animate_sprite_system(
-  time: Res<Time>,
-  texture_atlases: Res<Assets<TextureAtlas>>,
-  mut query: Query<(
-    &mut Timer,
-    &mut TextureAtlasSprite,
-    &Handle<TextureAtlas>,
-  )>,
-) {
-  for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
-    timer.tick(time.delta());
-    if timer.finished() {
-      let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
-      sprite.index =
-        ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
-    }
-  }
-}
-
-fn setup(
-  mut commands: Commands,
-  asset_server: Res<AssetServer>,
-  mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-) {
-  let texture_handle = asset_server.load("sprites/hero.png");
-  let texture_atlas =
-    TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 32.0), 4, 4);
-  let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
-  commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-  commands
-    .spawn_bundle(SpriteSheetBundle {
-      texture_atlas: texture_atlas_handle,
-      transform: Transform::from_scale(Vec3::splat(4.0)),
+    .insert_resource(bevy::log::LogSettings {
+      level: bevy::log::Level::DEBUG,
+      filter: "wgpu=warn,bevy_ecs=info".to_string(),
+    })
+    .insert_resource(WindowDescriptor {
+      title: "dungeon".to_string(),
+      width: 1280.0,
+      height: 720.0,
+      vsync: true,
       ..Default::default()
     })
-    .insert(Timer::from_seconds(0.25, true));
-}
-
-fn setup_tilemap(asset_server: Res<AssetServer>) {
-  let texture_handle = asset_server.load("tiles/tile.png");
-  if let Ok(_tilemap) = Tilemap::builder()
-    .texture_atlas(texture_handle)
-    .texture_dimensions(32, 32)
-    .dimensions(40, 30)
-    .finish()
-  {}
+    .add_plugins(DefaultPlugins)
+    .add_plugin(TilemapPlugin)
+    .add_plugin(loading::LoadingScreenPlugin)
+    .run();
 }
